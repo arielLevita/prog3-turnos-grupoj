f1ce5adaea23aabc320287442c9228d4664f37c2
wox9000 <105942978+wox9000@users.noreply.github.com>
2026-06-05 23:37:34 -0300

    Se corrige error de tipeo 'atentido' por 'atendido' en turnosDb.js

--- db/turnosDb.js
+++ db/turnosDb.js
@@ -3,7 +3,7 @@ import pool from "./conexion.js";
 export default class TurnosDb {
     
     buscarTodas = async (filters = null, limit = 0, offset = 0, order = null) => {
-        let strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atentido,
+        let strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atendido,
                              CONCAT(vm.apellido, ' ', vm.nombres) AS medico_nombre,
                              CONCAT(vp.apellido, ' ', vp.nombres) AS paciente_nombre,
                              os.nombre AS obra_social_nombre
@@ -39,7 +39,7 @@ export default class TurnosDb {
     }
 
     turnosDeUnMedico = async (id_usuario) => {
-        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atentido,
+        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atendido,
                              CONCAT(vm.apellido, ' ', vm.nombres) AS medico_nombre,
                              CONCAT(vp.apellido, ' ', vp.nombres) AS paciente_nombre,
                              os.nombre AS obra_social_nombre
@@ -53,7 +53,7 @@ export default class TurnosDb {
     }
 
     turnosDeUnPaciente = async (id_usuario) => {
-        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atentido,
+        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atendido,
                              CONCAT(vm.apellido, ' ', vm.nombres) AS medico_nombre,
                              CONCAT(vp.apellido, ' ', vp.nombres) AS paciente_nombre,
                              os.nombre AS obra_social_nombre
@@ -67,7 +67,7 @@ export default class TurnosDb {
     }
 
     buscarPorId = async (id) => {
-        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atentido,
+        const strSql = `SELECT t.id_turno_reserva, t.id_medico, t.id_paciente, t.id_obra_social, t.fecha_hora, t.valor_total, t.atendido,
                                CONCAT(vm.apellido, ' ', vm.nombres) AS medico_nombre,
                                CONCAT(vp.apellido, ' ', vp.nombres) AS paciente_nombre,
                                os.nombre AS obra_social_nombre
@@ -87,7 +87,7 @@ export default class TurnosDb {
             await conexion.beginTransaction();
 
             const strSql = `INSERT INTO turnos_reservas 
-                            (id_medico, id_paciente, id_obra_social, fecha_hora, valor_total, atentido) 
+                            (id_medico, id_paciente, id_obra_social, fecha_hora, valor_total, atendido) 
                             VALUES (?, ?, ?, ?, ?, 0)`;
             
             const [resultado] = await conexion.execute(strSql, [
