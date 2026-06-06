9957d39b379e84a7b9cb273de089968ca1526d80
wox9000 <105942978+wox9000@users.noreply.github.com>
2026-06-06 00:49:08 -0300

    Se registra nuevo endpoint de estadísticas en Swagger

--- documentacion/opcionesSwagger.js
+++ documentacion/opcionesSwagger.js
@@ -7,47 +7,55 @@ const especialidadesDocs = JSON.parse(fs.readFileSync(new URL('./especialidadesD
 const obrasSocialesDocs = JSON.parse(fs.readFileSync(new URL('./obrasSocialesDocs.json', import.meta.url), 'utf-8'));
 const pacientesDocs = JSON.parse(fs.readFileSync(new URL('./pacientesDocs.json', import.meta.url), 'utf-8'));
 const medicosDocs = JSON.parse(fs.readFileSync(new URL('./medicosDocs.json', import.meta.url), 'utf-8'));
+const estadisticasDocs = JSON.parse(
+  fs.readFileSync(new URL("./estadisticasDocs.json", import.meta.url), "utf-8"),
+);
 
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'API Turnos Grupo J',
-            version: '2.0.0',
-            description: 'Documentación interactiva de la API para el Trabajo Integrador'
-        },
-        servers: [{ url: `http://localhost:${process.env.PUERTO || 3007}` }],
-        
-        components: {
-            securitySchemes: {
-                bearerAuth: {
-                    type: 'http',
-                    scheme: 'bearer',
-                    bearerFormat: 'JWT',
-                }
-            },
-            schemas: {
-                ...usuariosDocs.components.schemas,
-                ...turnosDocs.components.schemas,
-                ...especialidadesDocs.components.schemas,
-                ...obrasSocialesDocs.components.schemas,
-                ...pacientesDocs.components.schemas,
-                ...medicosDocs.components.schemas
-            }
-        },
-        security: [{
-            bearerAuth: []
-        }],
-        paths: {
-            ...usuariosDocs.paths,
-            ...turnosDocs.paths,
-            ...especialidadesDocs.paths,
-            ...obrasSocialesDocs.paths,
-            ...pacientesDocs.paths,
-            ...medicosDocs.paths
-        }
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Turnos Grupo J",
+      version: "2.0.0",
+      description:
+        "Documentación interactiva de la API para el Trabajo Integrador",
     },
-    apis: [],
+    servers: [{ url: `http://localhost:${process.env.PUERTO || 3007}` }],
+
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+      schemas: {
+        ...usuariosDocs.components.schemas,
+        ...turnosDocs.components.schemas,
+        ...especialidadesDocs.components.schemas,
+        ...obrasSocialesDocs.components.schemas,
+        ...pacientesDocs.components.schemas,
+        ...medicosDocs.components.schemas,
+        ...estadisticasDocs.components.schemas,
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+    paths: {
+      ...usuariosDocs.paths,
+      ...turnosDocs.paths,
+      ...especialidadesDocs.paths,
+      ...obrasSocialesDocs.paths,
+      ...pacientesDocs.paths,
+      ...medicosDocs.paths,
+      ...estadisticasDocs.paths
+    },
+  },
+  apis: [],
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
