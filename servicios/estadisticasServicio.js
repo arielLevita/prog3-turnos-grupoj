e0fc5a379a28d3dad8834ee18e058d715755a439
wox9000 <105942978+wox9000@users.noreply.github.com>
2026-06-15 12:14:25 -0300

    Terminando funcionalidad de estadísticas

--- servicios/estadisticasServicio.js
+++ servicios/estadisticasServicio.js
@@ -2,23 +2,23 @@ import pool from "../db/conexion.js";
 
 export default class EstadisticasServicio {
   obtenerEstadisticas = async (reporte, params) => {
-    let spName = "";
+    let paNombre = "";
 
     switch (reporte) {
       case "obras-sociales":
-        spName = "pa_estadisticas_obras_sociales";
+        paNombre = "pa_estadisticas_obras_sociales";
         break;
       case "medicos":
-        spName = "pa_estadisticas_medicos";
+        paNombre = "pa_estadisticas_medicos";
         break;
       case "especialidades":
-        spName = "pa_estadisticas_especialidades";
+        paNombre = "pa_estadisticas_especialidades";
         break;
       default:
         throw new Error("Reporte no encontrado");
     }
 
-    const [resultados] = await pool.query(`CALL ${spName}(?, ?)`, params);
+    const [resultados] = await pool.query(`CALL ${paNombre}(?, ?)`, params);
     return resultados[0];
   };
 }
